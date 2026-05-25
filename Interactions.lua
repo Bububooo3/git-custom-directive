44cbd2b8294b9a37827028bba53baa56088a4562
Richard Scott <bububooo3@gmail.com>
2026-05-25 21:50:36 +0000

    e

--- Interactions.lua
+++ Interactions.lua
@@ -33,19 +33,30 @@ end
 			<config> // this is quick-config for local changes/setup
 				token: [TOKEN];
 				repository: [REPO];
-				directory: [GITHUB PATH];
+				path: [GITHUB PATH];
 				branch: [BRANCH];
 				output-enabled: [OUTPUT ENABLED];
 				commit-message: [COMMIT MESSAGE];
 			</config>
 
 			<files>
-				script: {
+				workspace.Script: {
 					name: "Example";
 
 					token: "12345678910";
 					repository: "username/reponame";
-					directory: ""; --<prefixes: ./, /, nothing>
+					path: "";
+					branch: "main";
+					output-enabled: false;
+					commit-message: "We did it!";
+				};
+
+				workspace.Folder: { // In the parser it will identify & report scripts automatically so that data is still {BaseScript}
+					name: "Example";
+
+					token: "12345678910";
+					repository: "username/reponame";
+					path: ""; --<prefixes: ./, /, nothing>
 					branch: "main";
 					output-enabled: false;
 					commit-message: "We did it!";
@@ -58,9 +69,10 @@ end
 type GitConfigData = {
 	token: string | nil;
 	repository: string | nil;
-	directory: string | nil;
+	path: string | nil;
 	branch: string | nil;
 	message: string | nil;
+	base: string;
 }
 
 type GitDirectiveData = {
@@ -71,14 +83,14 @@ type GitDirectiveData = {
 			name: string | nil;
 			token: string | nil;
 			repository: string | nil;
-			directory: string | nil;
+			path: string | nil;
 			branch: string | nil;
 			message: string | nil;
 		}
 	}
 }
 
-local gitConfigGlobal: GitConfigData = {}
+local gitConfigGlobal: GitConfigData = {base='main'}
 local baseURL = "https://api.github.com/repos/%s/contents/%s"
 
 function gitPush(data: GitDirectiveData)
@@ -87,25 +99,30 @@ function gitPush(data: GitDirectiveData)
 	local gitConfigLocal = data.config
 
 	for path, file in pairs(data.files) do
-		local split = path:split(".") -- {game, workspace, ModelName, PartName, ScriptName}
+		local split = path:split(".") -- {game, Workspace, ModelName, PartName, ScriptName}
 
-		local name = file.name or split[#split]
+		local name = file.name or split[#split] -- will have issues if there are stars in the instance names
 		local repository = file.repository or gitConfigLocal.repository or gitConfigGlobal.repository or nil
 		local token = file.token or gitConfigLocal.token or gitConfigGlobal.token or nil
-		local directory = file.directory or gitConfigLocal.directory or gitConfigGlobal.directory or ""
+		local filePath = file.path or gitConfigLocal.path or gitConfigGlobal.path or ""
 		local branch = file.branch or gitConfigLocal.branch or gitConfigGlobal.branch or "main"
 		local msg = file.message or gitConfigLocal.message or gitConfigGlobal.message or `Updated {name}`
 
+		local url = baseURL:format(repository, filePath)
+		local headers = {
+			["Authorization"] = `token {token}`,
+			["Accept"] = "application/vnd.github.v3+json"
+		}
 
 		-- Guards
 		local issues = 0
 		if not repository then
-			warn(`No repository specified for file {name}`)
+			warn(`(git-push) No repository specified for file {name}`)
 			issues += 1
 		end
 
 		if not token then
-			warn(`No token specified for file {name}`)
+			warn(`(git-push) No token specified for file {name}`)
 			issues += 1
 		end
 
@@ -124,67 +141,106 @@ function gitPush(data: GitDirectiveData)
 		elseif game:FindFirstChild(root) then
 			fileObject = game
 		else
-			warn(`Invalid file path for file {name}`)
+			warn(`(git-push) Invalid path for file {name}`)
 		end
 
 		for i, t in ipairs(split) do
 			fileObject = fileObject[t] or nil
 		end
 
+		if not fileObject:IsA("BaseScript") then 
+			warn(`(git-push) Invalid instance for file {name}`)
+			return
+		end
 		-- End of finding real script instance
 
+		-- Get SHA
+		local mySHA = nil
 
-		local url = baseURL:format(repository, directory)
-
-		local body = {
-			message = msg;
-			content = Functions.to_base64()
-		}
-	end
-end
-
-function Interactions.pushToGitHub(src: {BaseScript}, repo, targetLocation, srcLocation, branchFlag, outputFlag, messageFlag, nameFlag)
-	local scripts = src
-	local url = "https://api.github.com/repos/" ..repo .. "/contents/"
-
-	ChangeHistoryService:SetWaypoint("Before GitHub Push")
-
-	for name, data in pairs(scripts) do
-		task.wait()
-		local filePath = Functions.getScriptPath(data.Object)
-		local fileUrl = url .. filePath
-		
-		local requestBody = {
-			message = "Updated " .. filePath,
-			content = Functions.to_base64(data.Source),
-			branch = branchFlag or "main",
-			sha = Functions.getFileSHA(filePath) or Interactions.getLatestCommitSHA()
-		}
-
-		local headers = {
-			["Authorization"] = "token " .. plugin:GetSetting(Gitsync.Settings.TOKEN),
-			["Accept"] = "application/vnd.github.v3+json"
-		}
-
-		local success, response = pcall(function()
+		local success1, result = pcall(function()
 			return HttpService:RequestAsync({
-				Url = fileUrl,
-				Method = "PUT",
-				Headers = headers,
-				Body = HttpService:JSONEncode(requestBody)
+				Url = url..`?ref={branch}`,
+				Method = "GET",
+				Headers = headers
 			})
 		end)
 
-		if response.Success then
-			if plugin:GetSetting(Gitsync.Settings.OUTPUTENABLED) then print("Pushed: ", filePath) end
-			pushButton.ImageLabel.ImageColor3 = Gitsync.Colors.Green
-		else
-			warn("Failed to push: ", filePath)
-			warn("Response: ", response.Body)
-			pushButton.ImageLabel.ImageColor3 = Gitsync.Colors.Red
+		if success1 and result.Success then
+			local temp = HttpService:JSONDecode(result.Body)
+			mySHA = temp.sha
 		end
+		-- End of getting SHA
+		
+		-- Getting backup SHA
+		if not mySHA then
+			warn(`(git-push) Initial attempt to retrieve SHA failed for file {name}`)
+			warn(`(git-push) Attempting to create new branch from base branch for file {name}`)
 
-		ChangeHistoryService:SetWaypoint("After GitHub Push")
+			local success2, result2 = pcall(function()
+			    return HttpService:RequestAsync({
+			        Url = `https://api.github.com/repos/{repository}/git/refs/heads/{gitConfigGlobal.base}`,
+			        Method = "GET",
+			        Headers = headers
+			    })
+			end)
+
+			if success2 and result2.Success then
+			    mySHA = HttpService:JSONDecode(result2.Body).object.sha 
+			else
+			    warn(`(git-push) Failed to retrieve base SHA for file {name}`)
+			end
+
+			local requestBody = HttpService:JSONEncode({
+				ref = `refs/heads/{branch}`,
+				sha = mySHA
+			})
+
+			local success3, result3 = pcall(function()
+				return HttpService:RequestAsync({
+					Url = `https://api.github.com/repos/{repository}/git/refs`,
+					Method = "POST",
+					Headers = headers,
+					Body = requestBody
+				})
+			end)
+
+			if success3 and not result3.Success then
+				warn(`(git-push) Failed to create branch for file {name}: `.. result3.Body)
+				mySHA = nil
+			elseif not success3 then
+				warn(`(git-push) Failed to create branch for file {name}: (no data)`)
+				mySHA = nil
+			end
+		end
+		-- End of getting backup SHA
+
+		-- SHA guard
+		if not mySHA then
+			warn(`(git-push) Unable to obtain remote SHA for file {name}`)
+		end
+		-- End of SHA guard
+
+		-- Finally try and push the stuff
+		local success4, result4 = pcall(function()
+				return HttpService:RequestAsync({
+					Url = url,
+					Method = "PUT",
+					Headers = headers,
+					Body = HttpService:JSONEncode({
+						message = msg;
+						content = Functions.to_base64(fileObject.Source);
+						branch = branch;
+						sha = mySHA
+				})
+			})
+		end)
+
+		if success4 and not result4.Success then
+			warn(`(git-push) Failed to push file {name}: `.. result4.Body)
+		elseif not success4 then
+			warn(`(git-push) Failed to push file {name}: (no data)`)
+		end
+		-- End of finally tryna push the stuff
 	end
 end
 
--- main.lua
+++ main.lua
@@ -26,4 +26,213 @@
 
 ]]
 
-function get
+----> Convert data from base 10 to base 64
+function to_base64(data: any) : string -- (XDeltaXen) - https://devforum.roblox.com/t/base64-encoding-and-decoding-in-lua/1719860
+	local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+	return ((data:gsub('.', function(x) 
+		local r,b='',x:byte()
+		for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
+		return r;
+	end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
+		if (#x < 6) then return '' end
+		local c=0
+		for i=1,6 do c=c+(x:sub(i,i)=='1' and 2^(6-i) or 0) end
+		return b:sub(c+1,c+1)
+	end)..({ '', '==', '=' })[#data%3+1])
+end
+
+----> Convert data from base 64 to base 10
+function from_base64(data: any) : string -- (XDeltaXen) - https://devforum.roblox.com/t/base64-encoding-and-decoding-in-lua/1719860
+	local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+	data = string.gsub(data, '[^'..b..'=]', '')
+	return (data:gsub('.', function(x)
+		if (x == '=') then return '' end
+		local r,f='',(b:find(x)-1)
+		for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
+		return r;
+	end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+		if (#x ~= 8) then return '' end
+		local c=0
+		for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
+		return string.char(c)
+	end))
+end
+
+type GitConfigData = {
+	token: string | nil;
+	repository: string | nil;
+	path: string | nil;
+	branch: string | nil;
+	message: string | nil;
+	base: string;
+}
+
+type GitDirectiveData = {
+	config: GitConfigData,
+
+	files: { -- [script absolute file path from game]: {upload data}
+		[string]: {
+			name: string | nil;
+			token: string | nil;
+			repository: string | nil;
+			path: string | nil;
+			branch: string | nil;
+			message: string | nil;
+		}
+	}
+}
+
+local gitConfigGlobal: GitConfigData = {base='main'}
+local baseURL = "https://api.github.com/repos/%s/contents/%s"
+local HttpService = game:GetService("HttpService")
+
+function gitPush(data: GitDirectiveData)
+	-- post parsing
+
+	local gitConfigLocal = data.config
+
+	for path, file in pairs(data.files) do
+		local split = path:split(".") -- {game, Workspace, ModelName, PartName, ScriptName}
+
+		local name = file.name or split[#split] -- will have issues if there are stars in the instance names
+		local repository = file.repository or gitConfigLocal.repository or gitConfigGlobal.repository or nil
+		local token = file.token or gitConfigLocal.token or gitConfigGlobal.token or nil
+		local filePath = file.path or gitConfigLocal.path or gitConfigGlobal.path or ""
+		local branch = file.branch or gitConfigLocal.branch or gitConfigGlobal.branch or "main"
+		local msg = file.message or gitConfigLocal.message or gitConfigGlobal.message or `Updated {name}`
+
+		local url = baseURL:format(repository, filePath)
+		local headers = {
+			["Authorization"] = `token {token}`,
+			["Accept"] = "application/vnd.github.v3+json"
+		}
+
+		-- Guards
+		local issues = 0
+		if not repository then
+			warn(`(git-push) No repository specified for file {name}`)
+			issues += 1
+		end
+
+		if not token then
+			warn(`(git-push) No token specified for file {name}`)
+			issues += 1
+		end
+
+		if issues > 0 then return end
+		-- End of Guards
+
+		-- Find real script instance
+		local root = split[1]
+		local fileObject
+
+		if root == "game" then
+			fileObject = game
+			table.remove(split, 1)
+		elseif root == "script" then
+			fileObject = script
+		elseif game:FindFirstChild(root) then
+			fileObject = game
+		else
+			warn(`(git-push) Invalid path for file {name}`)
+		end
+
+		for i, t in ipairs(split) do
+			fileObject = fileObject[t] or nil
+		end
+
+		if not fileObject:IsA("BaseScript") then 
+			warn(`(git-push) Invalid instance for file {name}`)
+			return
+		end
+		-- End of finding real script instance
+
+		-- Get SHA
+		local mySHA = nil
+
+		local success1, result = pcall(function()
+			return HttpService:RequestAsync({
+				Url = url..`?ref={branch}`,
+				Method = "GET",
+				Headers = headers
+			})
+		end)
+
+		if success1 and result.Success then
+			local temp = HttpService:JSONDecode(result.Body)
+			mySHA = temp.sha
+		end
+		-- End of getting SHA
+		
+		-- Getting backup SHA
+		if not mySHA then
+			warn(`(git-push) Initial attempt to retrieve SHA failed for file {name}`)
+			warn(`(git-push) Attempting to create new branch from base branch for file {name}`)
+
+			local success2, result2 = pcall(function()
+			    return HttpService:RequestAsync({
+			        Url = `https://api.github.com/repos/{repository}/git/refs/heads/{gitConfigGlobal.base}`,
+			        Method = "GET",
+			        Headers = headers
+			    })
+			end)
+
+			if success2 and result2.Success then
+			    mySHA = HttpService:JSONDecode(result2.Body).object.sha 
+			else
+			    warn(`(git-push) Failed to retrieve base SHA for file {name}`)
+			end
+
+			local requestBody = HttpService:JSONEncode({
+				ref = `refs/heads/{branch}`,
+				sha = mySHA
+			})
+
+			local success3, result3 = pcall(function()
+				return HttpService:RequestAsync({
+					Url = `https://api.github.com/repos/{repository}/git/refs`,
+					Method = "POST",
+					Headers = headers,
+					Body = requestBody
+				})
+			end)
+
+			if success3 and not result3.Success then
+				warn(`(git-push) Failed to create branch for file {name}: `.. result3.Body)
+				mySHA = nil
+			elseif not success3 then
+				warn(`(git-push) Failed to create branch for file {name}: (no data)`)
+				mySHA = nil
+			end
+		end
+		-- End of getting backup SHA
+
+		-- SHA guard
+		if not mySHA then
+			warn(`(git-push) Unable to obtain remote SHA for file {name}`)
+		end
+		-- End of SHA guard
+
+		-- Finally try and push the stuff
+		local success4, result4 = pcall(function()
+				return HttpService:RequestAsync({
+					Url = url,
+					Method = "PUT",
+					Headers = headers,
+					Body = HttpService:JSONEncode({
+						message = msg;
+						content = to_base64(fileObject.Source);
+						branch = branch;
+						sha = mySHA
+				})
+			})
+		end)
+
+		if success4 and not result4.Success then
+			warn(`(git-push) Failed to push file {name}: `.. result4.Body)
+		elseif not success4 then
+			warn(`(git-push) Failed to push file {name}: (no data)`)
+		end
+		-- End of finally tryna push the stuff
+	end
+end
